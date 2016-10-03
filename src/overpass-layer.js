a1bf91f21e444ae8cf5096790efbba3080fa3e06
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-10-03 21:44:22 +0200

    Use eslint to format code after StandardJS

--- src/overpass-layer.js
+++ src/overpass-layer.js
@@ -1,40 +1,47 @@
+/* global overpassFrontend:false */
+
 var BoundingBox = require('boundingbox')
 var twig = require('twig').twig
+var OverpassFrontend = require('overpass-frontend')
 
-function OverpassLayer(query, options) {
-  if(!options)
+function OverpassLayer (query, options) {
+  var template
+
+  if (!options) {
     options = {}
+  }
 
   this.query = query
   this.overpassFrontend = 'overpassFrontend' in options ? options.overpassFrontend : overpassFrontend
   this.style = 'style' in options ? options.style : {}
   this.minZoom = 'minZoom' in options ? options.minZoom : 16
   this.maxZoom = 'maxZoom' in options ? options.maxZoom : null
-  this.featureTitle = 'featureTitle' in options ? options.featureTitle : function(ob) { return ob.tags.name || ob.tags.operator || ob.tags.ref || ob.id }
+  this.featureTitle = 'featureTitle' in options ? options.featureTitle : function (ob) { return ob.tags.name || ob.tags.operator || ob.tags.ref || ob.id }
   this.featureBody = 'featureBody' in options ? options.featureBody : ''
 
-  if(typeof this.featureTitle == 'string') {
-    var template = twig({ data: this.featureTitle })
-    this.featureTitle = function(template, ob) {
+  if (typeof this.featureTitle === 'string') {
+    template = twig({ data: this.featureTitle })
+    this.featureTitle = function (template, ob) {
       return template.render(ob)
     }.bind(this, template)
   }
-  if(typeof this.featureBody == 'string') {
-    var template = twig({ data: this.featureBody })
-    this.featureBody = function(template, ob) {
+  if (typeof this.featureBody === 'string') {
+    template = twig({ data: this.featureBody })
+    this.featureBody = function (template, ob) {
       return template.render(ob)
     }.bind(this, template)
   }
-  if(typeof this.style == 'string') {
-    var template = twig({ data: this.style })
-    this.style = function(template, ob) {
+  if (typeof this.style === 'string') {
+    template = twig({ data: this.style })
+    this.style = function (template, ob) {
       var str = template.render(ob).split('\n')
       var ret = {}
 
-      for(var i = 0; i < str.length; i++) {
-        var m;
-        if(m = str[i].match(/^\s*([a-zA-Z0-9_]+)\s*:\s*(.*)\s*$/))
+      for (var i = 0; i < str.length; i++) {
+        var m
+        if ((m = str[i].match(/^\s*([a-zA-Z0-9_]+)\s*:\s*(.*)\s*$/))) {
           ret[m[1]] = m[2]
+        }
       }
 
       return ret
@@ -44,20 +51,22 @@ function OverpassLayer(query, options) {
   this.visible_features = {}
 }
 
-OverpassLayer.prototype.addTo = function(map) {
+OverpassLayer.prototype.addTo = function (map) {
   this.map = map
   this.map.on('moveend', this.check_update_map.bind(this))
   this.check_update_map()
 }
 
-OverpassLayer.prototype.check_update_map = function() {
+OverpassLayer.prototype.check_update_map = function () {
   var bounds = new BoundingBox(this.map.getBounds())
+  var k
+  var ob
 
-  if(this.map.getZoom() < this.minZoom ||
+  if (this.map.getZoom() < this.minZoom ||
      (this.maxZoom !== null && this.map.getZoom() > this.maxZoom)) {
-    for (var k in this.visible_features) {
-      var ob = this.visible_features[k]
-      map.removeLayer(ob.feature)
+    for (k in this.visible_features) {
+      ob = this.visible_features[k]
+      this.map.removeLayer(ob.feature)
     }
 
     this.visible_features = {}
@@ -65,12 +74,12 @@ OverpassLayer.prototype.check_update_map = function() {
   }
 
   // Hide loaded but non-visible objects
-  for (var k in this.visible_features) {
-    var ob = this.visible_features[k]
+  for (k in this.visible_features) {
+    ob = this.visible_features[k]
 
     if (!ob.isVisible(bounds)) {
-      map.removeLayer(ob.feature)
-      delete(this.visible_features[k])
+      this.map.removeLayer(ob.feature)
+      delete this.visible_features[k]
     }
   }
 
@@ -86,27 +95,30 @@ OverpassLayer.prototype.check_update_map = function() {
     function (err, ob) {
       if (!ob.feature) {
         var style = this.style
-        if(typeof this.style == 'function')
+        if (typeof this.style === 'function') {
           style = this.style(ob)
+        }
 
         ob.feature = ob.leafletFeature(style)
 
-        var popup_content = ''
+        var popupContent = ''
 
-        if(typeof this.featureTitle == 'function')
-          popup_content += '<h1>' + this.featureTitle(ob) + '</h1>'
-        else
-          popup_content += this.featureTitle
+        if (typeof this.featureTitle === 'function') {
+          popupContent += '<h1>' + this.featureTitle(ob) + '</h1>'
+        } else {
+          popupContent += this.featureTitle
+        }
 
-        if(typeof this.featureBody == 'function')
-          popup_content += '<h1>' + this.featureBody(ob) + '</h1>'
-        else
-          popup_content += this.featureBody
+        if (typeof this.featureBody === 'function') {
+          popupContent += '<h1>' + this.featureBody(ob) + '</h1>'
+        } else {
+          popupContent += this.featureBody
+        }
 
-        ob.feature.bindPopup(popup_content)
+        ob.feature.bindPopup(popupContent)
       }
-      
-      ob.feature.addTo(map)
+
+      ob.feature.addTo(this.map)
       this.visible_features[ob.id] = ob
     }.bind(this),
     function (err) {
@@ -114,7 +126,9 @@ OverpassLayer.prototype.check_update_map = function() {
   )
 }
 
-if(typeof module != 'undefined' && module.exports)
+if (typeof module !== 'undefined' && module.exports) {
   module.exports = OverpassLayer
-if(typeof window != 'undefined')
+}
+if (typeof window !== 'undefined') {
   window.OverpassLayer = OverpassLayer
+}
