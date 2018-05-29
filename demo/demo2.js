8e1255dd5a22ef379b0e5aebac6bced2df6a26c0
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-05-29 21:57:39 +0200

    demo-lint

--- demo/demo2.js
+++ demo/demo2.js
@@ -1,29 +1,31 @@
 document.body.classList.add('hasInfo')
 
-var overpass_layer2 = new OverpassLayer({
+var overpassLayer = new OverpassLayer({
   query: '(way[leisure=park];relation[leisure=park];);',
   minZoom: 14,
   feature: {
-    style: function(ob) {
+    style: function (ob) {
       return {
-          nodeFeature: 'CircleMarker',
-          color: 'green',
-          fillColor: 'green',
-          fillOpacity: 0.2,
-          width: 1,
-          radius: 6
+        nodeFeature: 'CircleMarker',
+        color: 'green',
+        fillColor: 'green',
+        fillOpacity: 0.2,
+        width: 1,
+        radius: 6
       }
     },
-    body: function(ob) {
+    body: function (ob) {
       return '<pre>' + JSON.stringify(ob.tags, null, '  ') + '</pre>'
     }
   }
 })
-overpass_layer2.onLoadStart = function (ev) {
+
+overpassLayer.onLoadStart = function (ev) {
   document.body.classList.add('loading')
 }
-overpass_layer2.onLoadEnd = function (ev) {
+overpassLayer.onLoadEnd = function (ev) {
   document.body.classList.remove('loading')
 }
-overpass_layer2.addTo(map)
-new OverpassLayerList(document.getElementById('info'), overpass_layer2);
+overpassLayer.addTo(map)
+
+var overpassLayerList = new OverpassLayerList(document.getElementById('info'), overpassLayer)
