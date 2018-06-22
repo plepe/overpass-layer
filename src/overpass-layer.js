1bed5d6061e431dd49932faa29f7ecf500035278
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-06-22 07:01:00 +0200

    Move strToStyle to Sublayer

--- src/overpass-layer.js
+++ src/overpass-layer.js
@@ -261,26 +261,6 @@ OverpassLayer.prototype.hide = function (id) {
   this.mainlayer.hide(id)
 }
 
-function strToStyle (style) {
-  var str = style.split('\n')
-  style = {}
-
-  for (var i = 0; i < str.length; i++) {
-    var m
-    if ((m = str[i].match(/^\s*([a-zA-Z0-9_]+)\s*:\s*(.*)\s*$/))) {
-      var v = m[2].trim()
-
-      if (v.match(/^-?[0-9]+(\.[0-9]+)?/)) {
-        v = parseFloat(v)
-      }
-
-      style[m[1]] = v
-    }
-  }
-
-  return style
-}
-
 // to enable extending twig
 OverpassLayer.twig = twig
 
