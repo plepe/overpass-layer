e39d3b9b59e8fb1a02670bda032640fcb28f61b7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-06-24 22:29:47 +0200

    Memberlayer has id 'member:' + layerid

--- src/overpass-layer.js
+++ src/overpass-layer.js
@@ -51,6 +51,7 @@ function OverpassLayer (options) {
     this.options.queryOptions.members = true
 
     let memberOptions = {
+      id: 'member:' + this.options.id,
       minZoom: this.options.minZoom,
       maxZoom: this.options.maxZoom,
       feature: this.options.memberFeature,
