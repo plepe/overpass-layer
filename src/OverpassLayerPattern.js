5f59bb75e4703d59674ca22fbd74489165991e08
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-10-29 20:03:36 +0100

    OverpassLayerPattern: Show arrowHeads if style has set them

--- src/OverpassLayerPattern.js
+++ src/OverpassLayerPattern.js
@@ -10,6 +10,41 @@ class OverpassLayerPattern {
   }
 
   processObject (object, data) {
+    for (var k in data.features) {
+      let def = k === 'default' ? data.data.style : data.data['style:' + k]
+
+      if ('pattern' in def) {
+        let symbol
+        let pathOptions = {}
+        let options = {}
+
+        for (let k in def) {
+          let m1 = k.match(/^pattern-path-(.*)$/)
+          let m2 = k.match(/^pattern-(.*)$/)
+
+          if (m1) {
+            pathOptions[m1[1]] = def[k]
+          } else if (m2) {
+            options[m2[1]] = def[k]
+          }
+        }
+
+        switch (def.pattern.toString()) {
+          case 'arrowHead':
+            options.pathOptions = pathOptions
+            symbol = L.Symbol.arrowHead(options)
+            break
+          default:
+            // TODO
+        }
+
+        options.symbol = symbol
+        let pattern = L.polylineDecorator(data.features[k])
+        pattern.addTo(this.layer.map)
+
+        pattern.setPatterns([ options ])
+      }
+    }
   }
 }
 
