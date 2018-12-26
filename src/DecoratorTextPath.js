b3fcb28154314f031c7c09b130f6428fbe238b5a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-12-26 11:35:34 +0100

    DecoratorTextPath: text, textTurned: parse html string

--- src/DecoratorTextPath.js
+++ src/DecoratorTextPath.js
@@ -1,3 +1,45 @@
+function node2textpath (dom) {
+  if (dom.nodeType === 3) {
+    return dom.textContent
+  }
+
+  if (dom.nodeType !== 1) {
+    return ''
+  }
+
+  let ret = { text: [] }
+  for (let i = 0; i < dom.attributes.length; i++) {
+    let attr = dom.attributes.item(i)
+    ret[attr.name] = attr.value
+  }
+
+  let current = dom.firstChild
+  while (current) {
+    ret.text.push(node2textpath(current))
+    current = current.nextSibling
+  }
+
+  return ret
+}
+
+function html2textpath (str) {
+  if (typeof str !== 'string') {
+    return str
+  }
+
+  let ret = []
+  let div = document.createElement('div')
+  div.innerHTML = str
+
+  let current = div.firstChild
+  while (current) {
+    ret.push(node2textpath(current))
+    current = current.nextSibling
+  }
+
+  return ret
+}
+
 class DecoratorTextPath {
   constructor (layer) {
     this.layer = layer
@@ -14,14 +56,14 @@ class DecoratorTextPath {
   processObjectStyle (object, data, styleId, style) {
     if ('text' in style && 'setText' in data.features[styleId]) {
       data.features[styleId].setText(null)
-      data.features[styleId].setText(style.text, {
+      data.features[styleId].setText(html2textpath(style.text), {
         repeat: style.textRepeat,
         offset: style.textOffset,
         below: style.textBelow,
         orientation: style.textOrientation,
         allowCrop: false,
         center: style.textCenter,
-        turnedText: style.textTurned,
+        turnedText: html2textpath(style.textTurned),
         attributes: {
           'fill': style.textFill,
           'fill-opacity': style.textFillOpacity,
