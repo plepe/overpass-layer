2a61d3df9b466019a852cc09e5837f13bcf9980a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-04-28 18:43:01 +0200

    Group: start implementing GroupObject

--- src/GroupLayer.js
+++ src/GroupLayer.js
@@ -1,12 +1,50 @@
 const Sublayer = require('./Sublayer')
+const GroupObject = require('./GroupObject')
 
 class GroupLayer extends Sublayer {
-  add (group, feature) {
-    console.log('add', group, feature)
+  constructor (master, options) {
+    super(master, options)
+  }
+
+  add (group, member) {
+    let feature
+    let isNew = false
+
+    if (group in this.visibleFeatures) {
+      feature = this.visibleFeatures[group]
+    } else {
+      const groupObject = new GroupObject(group)
+      feature = new this.featureClass(groupObject, this)
+      this.visibleFeatures[group] = feature
+      isNew = true
+    }
+
+    feature.object.add(member)
+    feature.processObject()
+
+    if (isNew) {
+      feature.show()
+
+      if (this.master.onAppear) {
+        this.master.onAppear(feature)
+      }
+
+      this.master.emit('add', feature.object, feature)
+      this.emit('add', feature.object, feature)
+    }
   }
 
   remove (group, feature) {
-    console.log('remove', group, feature)
+    console.log('remove', group, feature.id)
+  }
+
+  recalc () {
+    console.log('recalc called')
+    super.recalc()
+  }
+
+  finishAdding () {
+    console.log('visible', Object.keys(this.visibleFeatures))
   }
 }
 
---
+++ src/GroupObject.js
@@ -0,0 +1,76 @@
+class GroupObject {
+  constructor (id) {
+    this.id = id
+    this.properties = 0
+    this.list = {}
+  }
+
+  memberIds () {
+  }
+
+  title () {
+    return this.id
+  }
+
+  GeoJSON () {
+    return {
+      type: 'FeatureCollection',
+      id: this.id,
+      properties: {},
+      features: Object.values(this.list).map(f => f.object.GeoJSON())
+    }
+  }
+
+  intersects (bbox) {
+    console.log('intersects called')
+    return 2
+  }
+
+  leafletFeature (options) {
+    if (!('shiftWorld' in options)) {
+      options.shiftWorld = [0, 0]
+    }
+
+    // no geometry? use the member features instead
+    if (!this.geometry) {
+      const feature = L.featureGroup()
+      feature._updateCallbacks = []
+
+      return feature
+    }
+
+    return feature
+  }
+
+  add (feature) {
+    this.list[feature.id] = feature
+
+    this.tags = {}
+    Object.values(this.list).forEach(item => {
+      for (let k in item.object.tags) {
+        if (!(k in this.tags)) {
+          this.tags[k] = {}
+        }
+        let values = item.object.tags[k].split(/;/g)
+        values.forEach(v => {
+          this.tags[k][v] = true
+        })
+      }
+    })
+
+    for (let k in this.tags) {
+      this.tags[k] = Object.keys(this.tags[k]).join(';')
+    }
+
+    this.meta = {}
+    this.meta.ids = Object.values(this.list)
+      .map(item => item.id)
+      .join(';')
+  }
+
+  remove (feature) {
+    delete this.list[feature.id]
+  }
+}
+
+module.exports = GroupObject
