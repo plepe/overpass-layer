f6598f18c6dbcc4097b3df42e729e31dcbfb140e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-05-03 19:34:39 +0200

    pointOnFeature: support FeatureCollection

--- src/pointOnFeature.js
+++ src/pointOnFeature.js
@@ -1,11 +1,25 @@
 const turf = {
   along: require('@turf/along').default,
+  center: require('@turf/center').default,
+  distance: require('@turf/distance').default,
   length: require('@turf/length').default,
   pointOnFeature: require('@turf/point-on-feature').default
 }
 
 function pointOnGeoJSON (geojson) {
-  if (geojson.geometry.type === 'LineString') {
+  if (geojson.type === 'FeatureCollection') {
+    if (!geojson.features.length) {
+      return null
+    }
+
+    const pois = geojson.features
+      .map(f => pointOnGeoJSON(f))
+      .filter(p => p)
+    const centroid = turf.center(geojson)
+    const dists = pois.map(p => turf.distance(centroid, p))
+    const minIndex = dists.indexOf(Math.min(...dists))
+    return pois[minIndex]
+  } else if (geojson.geometry.type === 'LineString') {
     return turf.along(geojson, turf.length(geojson) / 2)
   } else if (geojson.geometry.type === 'GeometryCollection' && geojson.geometry.geometries.length === 0) {
     return null
